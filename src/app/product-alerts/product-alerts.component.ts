e95a70e404b8ef263469a21b3222faec3cb7d6f6
le2kbqoahhkv68 <d.mansofernandez@gmail.com>
2020-04-30 10:27:15 +0200

    Events from child components

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
-export class ProductAlertsComponent implements OnInit {
+export class ProductAlertsComponent {
+  /** Input */
   @Input() product;
-  constructor() { }
 
-  ngOnInit() {
+  /** Output */
+  @Output() notify = new EventEmitter();
+  
+  /** Getters */
+  get showNotifyMe(): boolean {
+    return this.product.price > 700
   }
 
+  constructor() { }
 }
